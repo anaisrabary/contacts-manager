77b5232820e4d5313af68142fa4ee56f663a6bfd
anaisrabary <anais.rabary@mail.com>
2017-11-22 16:02:34 +0100

    Test Read CSV. ATTENTION Tests à Finir (TODO)

---
+++ src/test/java/com/anaisrabary/insa/ManagerCSVTests.java
@@ -0,0 +1,71 @@
+package com.anaisrabary.insa;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.StringJoiner;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ManagerCSVTests {
+    private static final String FILE_TMP = "tmp.csv";
+    private static final String FIELD_OUTPUT_SEPARATOR = ", ";
+
+    private static final String NICOLE_FERRONI_NAME = "Nicole Ferroni";
+    private static final String NICOLE_FERRONI_EMAIL = "contact@nicoleferroni.fr";
+    private static final String NICOLE_FERRONI_PHONE_NUMBER = "0651387945";
+
+    private static final String GUILLAUME_MEURICE_NAME = "Guillaume Meurice";
+    private static final String GUILLAUME_MEURICE_EMAIL = "contact@guillaumemeurice.fr";
+    private static final String GUILLAUME_MEURICE_PHONE_NUMBER = "0615389254";
+
+    private ByteArrayOutputStream out;
+
+    @Before
+    public void setUp() {
+        out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+    }
+
+    @After
+    public void tearDouw() throws Exception{
+        new File(FILE_TMP).delete();
+    }
+
+    @Test
+    public void Test_when_Read_contacts_from_file()throws Exception{
+       String contact1= new StringJoiner(FIELD_OUTPUT_SEPARATOR)
+               .add(NICOLE_FERRONI_NAME)
+               .add(NICOLE_FERRONI_EMAIL)
+               .add(NICOLE_FERRONI_PHONE_NUMBER).toString();
+        String contact2 = new StringJoiner(FIELD_OUTPUT_SEPARATOR)
+                .add(GUILLAUME_MEURICE_NAME)
+                .add(GUILLAUME_MEURICE_EMAIL).toString();
+
+        new FileOutputStream(new File(FILE_TMP)).write((contact1+"\n"+ contact2).getBytes());
+
+        ContactsManager cm = new ManagerCSV(FILE_TMP).readData();
+        cm.addContact(NICOLE_FERRONI_NAME,NICOLE_FERRONI_EMAIL,NICOLE_FERRONI_PHONE_NUMBER);
+        cm.addContact(GUILLAUME_MEURICE_NAME,GUILLAUME_MEURICE_EMAIL, null);
+
+        cm.printAllContacts();
+
+        String contactInfo1= NICOLE_FERRONI_NAME + FIELD_OUTPUT_SEPARATOR + NICOLE_FERRONI_EMAIL + FIELD_OUTPUT_SEPARATOR + NICOLE_FERRONI_PHONE_NUMBER;
+        String contactInfo2 = GUILLAUME_MEURICE_NAME + FIELD_OUTPUT_SEPARATOR + GUILLAUME_MEURICE_EMAIL;
+        assertThat(out.toString(), containsString(contactInfo1));
+        assertThat(out.toString(), containsString(contactInfo2));
+
+        assertThat(cm.recupererTousContacts().size(),equalTo(2));
+
+    }
+    // TODO : implement les tests pour vérifier le bon fonctionnement de manager CSV en read et write
+
+
+}
--- src/test/java/com/anaisrabary/insa/UITest.java
+++ src/test/java/com/anaisrabary/insa/UITest.java
@@ -21,7 +21,7 @@ public class UITest {
     public void Tester_UI_when_ajouter_contact_and_find_contact ()throws Exception{
         ContactsManager contactsManager = mock(ContactsManager.class);
         ManagerCSV fileCSV = mock(ManagerCSV.class);
-        String entree = "add\n"+NICOLE_FERRONI_NAME+"\n"+NICOLE_FERRONI_EMAIL+"\n"+NICOLE_FERRONI_PHONE_NUMBER+"\nfind\nnicole\nexit\n";
+        String entree = "add\n"+NICOLE_FERRONI_NAME+"\n"+NICOLE_FERRONI_EMAIL+"\n"+NICOLE_FERRONI_PHONE_NUMBER+"\nfind\nnicol\nexit\n";
         System.setIn(new ByteArrayInputStream(entree.getBytes()));
 
         when(fileCSV.readData()).thenReturn(contactsManager);
@@ -55,12 +55,15 @@ public class UITest {
     }
 
 
-    // TODO : finir les tests de UI ATTENTION : faire du JUnit aussi
+    // TODO : finir les tests de UI ATTENTION : faire du JUnit aussi ++++++ TEster openCSV
     @Test
     public void Tester_UI_when_find_contact_not_added() throws Exception{System.out.println("TODO") ;}
 
     @Test
     public void tester_UI_Ajouter_mauvais_contact()throws Exception{System.out.println("TODO") ;}
+
+    @Test
+    public void tester_UI_when_une_autre_commande()throws Exception{System.out.println("TODO") ;}
 
 }
 
